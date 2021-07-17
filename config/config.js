3cc6951d533d5c99839264e03acd3d504ec57870
Gabriela Ciríaco <gabs.ciriaco@gmail.com>
2021-07-17 19:17:33 -0300

    Adicionando .env e corrigindo porta do servidor

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const configuracoes = {
   urlImoveis: process.env.URL_IMOVEIS,
-  porta: process.env.PORTA || 5000,
+  porta: process.env.PORT || 5000,
 };
 
 export default configuracoes;
